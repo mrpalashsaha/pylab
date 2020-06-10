51ee88389f86183d0db7190bb3d2bec7d20704e7
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-10 04:31:26 +0300

    Create firstGraph.py

---
+++ firstGraph.py
@@ -0,0 +1,22 @@
+import pylab
+
+    pylab.plot ( [1,2,3,4] , [1,2,3,4]) 
+    pylab.plot ([1,2,3,4], [5,6,7,8]
+    pylab.show()
+    
+    
+pylab.figure_One // directly name the figure 
+
+pylab.plot ([1,2,3,4,], [1,2,3,4])
+
+
+pylab.figure_two
+
+pylab.plot(1,4,2,3], [5,6,7,8])
+
+pylab.savefig // directly save to the current file as firstsaved//
+
+pylab.show()
+
+
+
